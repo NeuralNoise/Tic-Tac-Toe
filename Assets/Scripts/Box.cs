6204758da81fe593b5abb7503e6f129cf4457dd0
DESKTOP-DF2FGDR\Adrien <adrien.gannerie@gmail.com>
2017-01-12 09:57:44 +0100

    Lancement du projet

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Box : MonoBehaviour
+{
+    public enum StateEnum { Empty, Cross, Cercle}
+    StateEnum state;
+    public StateEnum State
+    {
+        get { return state; }
+        set
+        {
+            state = value;
+            switch(state)
+            {
+                case StateEnum.Empty:
+                    transform.GetChild(0).gameObject.SetActive(false);
+                    transform.GetChild(1).gameObject.SetActive(false);
+                    transform.GetComponent<Button>().interactable = true;
+                    break;
+                case StateEnum.Cross:
+                    transform.GetChild(0).gameObject.SetActive(false);
+                    transform.GetChild(1).gameObject.SetActive(true);
+                    transform.GetComponent<Button>().interactable = false;
+                    break;
+                case StateEnum.Cercle:
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    transform.GetChild(1).gameObject.SetActive(false);
+                    transform.GetComponent<Button>().interactable = false;
+                    break;
+            }
+        }
+    }
+
+}
